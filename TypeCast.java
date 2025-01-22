f41900fee53f0c507ef731b94c96b30effacd3e7
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-22 10:28:37 +0530

    type casting

---
+++ TypeCast.java
@@ -0,0 +1,9 @@
+public class TypeCast {
+    public static void main (String args[   ]){
+        
+       
+        float f = 7.9f;
+        int q = (int) f;
+        System.out.print(q);
+    }    
+}
