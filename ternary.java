7ddeb706d8d3855a84a524920cd3d778b2150b84
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-23 10:21:41 +0530

    ternary

---
+++ ternary.java
@@ -0,0 +1,8 @@
+public class ternary {
+    public static void main  (String args[]){
+        int n =90;
+        int result;
+        result = n%2==0 ? 0 : 1;
+        System.out.print(result);
+    }
+}
