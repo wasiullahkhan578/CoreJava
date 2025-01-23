688ed3247702201d64baf40f56588ea8421604cc
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-23 14:33:46 +0530

    do while in java

---
+++ doWhile.java
@@ -0,0 +1,9 @@
+public class doWhile {
+    public static void main(String args[]){
+        int i= 1;
+        do{
+            System.out.println("hi "+ i);
+            i++;
+        }while(i<=4);
+    }
+}
