54131fc00866e68c4c40b92fb6edf2e3d32e7d7a
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-23 10:06:35 +0530

    statements

---
+++ statements.java
@@ -0,0 +1,11 @@
+public class statements {
+    public static void main (String args[]){
+        int x =8;
+        int y =9;
+        if( x<y)
+            System.out.print("hello");
+        else
+            System.out.print("bye");
+
+    }    
+}
