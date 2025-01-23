af5cfb701c06b103ff1d5da297c4d4de41bde71a
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-23 14:09:43 +0530

    while loop

---
+++ while1.java
@@ -0,0 +1,11 @@
+public class while1 {
+    public static void main (String args[]){
+        int i = 1;
+        while (i<=4){
+            System.out.println(i   +"  hii" );
+            i++;
+        }
+
+        System.out.println(i+ "  bye");
+    }
+}
