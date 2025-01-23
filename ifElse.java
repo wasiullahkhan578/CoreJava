c4221bcbee6970c4e3b5c0bec9f64d3e40f6a02e
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-23 10:10:06 +0530

    if else statements

---
+++ ifElse.java
@@ -0,0 +1,13 @@
+public class ifElse {
+    public static void main(String args[]){
+        int x = 9;
+        int y = 18;
+        int z = 27;
+        if(x>y && x>z)
+            System.out.print(x);
+        else if(y>z)
+            System.out.print(y);
+        else
+            System.out.print(z);
+    }
+}
