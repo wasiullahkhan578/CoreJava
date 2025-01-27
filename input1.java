c803f88095a8ea6772ab492c4ab134f57e234af9
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-27 11:14:32 +0530

    how to take input and print in output from user

---
+++ input1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class input1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String name = sc.next();
+        System.out.println(name);
+        int number = sc.nextInt();
+        System.out.println(number);
+        float price = sc.nextFloat();
+        System.out.println(price);
+        sc.close();
+    }
+}
