e295aed5f32b5352c38357a5e6fe1306b9ec4384
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-27 19:23:39 +0530

    isn prime or not

--- reverse.java
+++ reverse.java
@@ -4,12 +4,14 @@ public class reverse {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter  your number: ");
         int num = sc.nextInt();
+        int rev =0;
         while(num>0){
             int lastDigit = num%10;
-            System.out.print(lastDigit);
+            rev = (rev *10) + lastDigit;
             num = num/10;
 
         }
+        System.out.print(rev);
         sc.close();
     }
 }
