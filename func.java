d8e0696505e58057b680a384236abbec5084880d
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-28 17:29:21 +0530

    fn/methods

---
+++ func.java
@@ -0,0 +1,8 @@
+public class func {
+    public static void hello(){
+        System.out.println("Hello Functions / methods...");
+    }
+    public static void main(String[] args) {
+        hello();
+    }
+}
