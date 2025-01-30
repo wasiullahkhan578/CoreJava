0b92aba77b4322b6a66db8c4d4ce4cee6c5f246a
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-01-30 10:35:36 +0530

    fn taking input as parameter

---
+++ fnParameter.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class fnParameter {
+
+    public static int add( int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = add(a,b);
+        System.out.println(c);
+        sc.close();
+    }
+}
