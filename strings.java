1b00fd52ced8c37eee0a020a61044a96cca3651a
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-02-04 11:58:39 +0530

    string  creation and its obj by java

---
+++ strings.java
@@ -0,0 +1,9 @@
+public class strings{
+    public static void main(String[] args) {
+        String name = new String("farhan"); // string is actually is a class..
+        System.out.println(name);
+        System.out.println(name.concat(" khan"));
+        String name1 = "wasiullah khan"; // however we can directly assign the value  and java automatically create a object of it.
+        System.out.println(name1);
+    }
+}
