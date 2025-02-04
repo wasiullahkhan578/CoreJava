1d1a7b5b5119be034b0a82cf3d1f22371e0cc7e4
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-02-04 12:09:48 +0530

    string constatnt pool

--- strings.java
+++ strings.java
@@ -5,5 +5,10 @@ public class strings{
         System.out.println(name.concat(" khan"));
         String name1 = "wasiullah khan"; // however we can directly assign the value  and java automatically create a object of it.
         System.out.println(name1);
+
+
+        String s1 = "farhan";
+        String s2 = "farhan";
+        System.out.println(s1 == s2);// true because both variable hold the same data it is not obtain new space in the heap memory because of string constant pool
     }
 }
