83e2c193540a4326813e7a5e60d9ca24d0d02240
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-02-08 14:18:46 +0530

    math class

---
+++ MathExample.java
@@ -0,0 +1,14 @@
+public class MathExample {
+    public static void main(String[] args) {
+        int i = 20;
+        int j = 30;
+        int max = Math.max(i, j);
+        
+        System.out.println("The maximum of " + i + " and " + j + " is: " + max);
+
+        int min = Math.min(i, j);
+        
+        System.out.println("The minimum of " + i + " and " + j + " is: " + min);
+
+    }
+}
