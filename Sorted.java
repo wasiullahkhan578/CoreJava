7b06969e8408127bcb93c31f7c393bb22cd69fc1
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-02-14 19:30:04 +0530

    search in a sorted 2d array

---
+++ Sorted.java
@@ -0,0 +1,24 @@
+public class Sorted {
+    public static void main(String[] args) {
+        int matrix [][] = {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+        int key = 39;
+        search(matrix,key);
+        
+    }
+    public static boolean search( int matrix [][], int key){
+        int row =0;
+        int col = matrix[0].length-1;
+        while (row < matrix.length &&col>=0) {
+            if(matrix[row][col] == key){
+                System.out.println("found key at: ("+row+","+col+")");
+                return true;
+            }else if(key <matrix[row][col]){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        System.out.println(" key not found..");
+        return false;
+    }
+}
