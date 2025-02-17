b64393a1f9fa072bee8dcdcb43575e585cb9f186
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-02-17 12:31:16 +0530

    this is bitwise operator

---
+++ BitWise.java
@@ -0,0 +1,17 @@
+public class BitWise{
+    public static void main(String [] args){
+        System.out.println("this are bitwise operator: ");
+        System.out.println("this is Binary AND: (5&6) ");
+        System.out.println((5&6));
+        System.out.println("this is Binary OR: (5|6) ");
+        System.out.println((5|6));
+        System.out.println("this is Binary XOR: (5^6) ");
+        System.out.println((5^6));
+        System.out.println("this is Binary One's Compliment / NOT operator: (~0) ");
+        System.out.println((~0));
+        System.out.println("this is Lest Shift Operator:(5<<2) ");
+        System.out.println((5<<2));
+        System.out.println("this is Right Shift Operator: (6>>1) ");
+        System.out.println((6>>1));
+    }
+}
