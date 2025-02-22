2f0f399fb9cbf809c7e3ad1e782c4b00e928882b
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2025-02-22 19:18:00 +0530

    construtor calling

--- Abstraction.java
+++ Abstraction.java
@@ -3,12 +3,20 @@ public class Abstraction {
         Chicken c = new Chicken();
         c.eat();
         c.walk();
+        
         Horse h = new Horse();
         h.eat();
-        h.walk();        
+        h.walk();  
+          
+        
+        Mustang myHorse = new Mustang(); // constructor called  - animal-> horse-> mustang
     }
 }
 abstract class Animal{
+    String color;
+    Animal(){
+        System.out.println("Animal constructor celled..");
+    }
     void eat (){
         System.out.println("animal eats..");
     }
@@ -16,12 +24,32 @@ abstract class Animal{
 }
 
 class Horse extends Animal{
+    Horse(){
+        System.out.println("horse constructor called ..");
+    }
+
+    void changeColor(){
+        color = "black";
+    }
+
     void walk(){
         System.out.println("horse walks on 4 leg.");
     }
 }
+
+class Mustang extends Horse{
+    Mustang(){
+        System.out.println("Mustang constructor is called..");
+    }
+    
+}
+
 class Chicken extends Animal{
     void walk(){
         System.out.println(" chicken walks on 2 leg.");
     }
+    void changeColor(){
+        color = "white";
+    }
+
 }
